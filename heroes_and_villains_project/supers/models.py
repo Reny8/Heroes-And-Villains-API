8e13275a3e2397063c8c6a837903d4df3dabf1f8
Reny Reyes <renyreyes8@hotmail.com>
2022-04-04 14:44:41 -0400

    Created the serializer class for super

--- heroes_and_villains_project/supers/models.py
+++ heroes_and_villains_project/supers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from super_types.models import Super_Type
 # Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length = 50)
+    alter_ego = models.CharField(max_length = 50)
+    primary_ability = models.CharField(max_length = 50)
+    secondary_ability = models.CharField(max_length = 50)
+    catchphrase = models.CharField(max_length = 100)
+    super_type = models.ForeignKey(Super_Type, on_delete = models.CASCADE)
