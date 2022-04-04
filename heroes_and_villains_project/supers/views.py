a4aef810a3f385855ec668079ca3afaa4fc13556
Reny Reyes <renyreyes8@hotmail.com>
2022-04-04 15:17:26 -0400

    Registered super to admin

--- heroes_and_villains_project/supers/views.py
+++ heroes_and_villains_project/supers/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Super
+from .serializers import SuperSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def supers_list(request):
+    if request.method == 'GET':
+        super = Super.objects.all()
+        serializer = SuperSerializer(super,many = True)
+        return Response(serializer.data)
+        
