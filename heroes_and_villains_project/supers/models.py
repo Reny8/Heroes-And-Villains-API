85f18fe982e5af2fca8d16f5d3982ade5c992b02
Reny Reyes <renyreyes8@hotmail.com>
2022-04-04 16:32:16 -0400

    Completed the functionality for GET POST PUT and DELETE request + Completed creating the two functions and the urls

--- heroes_and_villains_project/supers/models.py
+++ heroes_and_villains_project/supers/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from super_types.models import Super_Type
 # Create your models here.
 class Super(models.Model):
-    name = models.CharField(max_length = 50)
-    alter_ego = models.CharField(max_length = 50)
-    primary_ability = models.CharField(max_length = 50)
-    secondary_ability = models.CharField(max_length = 50)
+    name = models.CharField(max_length = 100)
+    alter_ego = models.CharField(max_length = 100)
+    primary_ability = models.CharField(max_length = 100)
+    secondary_ability = models.CharField(max_length = 100)
     catchphrase = models.CharField(max_length = 100)
     super_type = models.ForeignKey(Super_Type, on_delete = models.CASCADE)
