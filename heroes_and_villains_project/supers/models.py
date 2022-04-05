1e4f5f429b5309ac871c00376270a456fc1c99b8
Reny Reyes <renyreyes8@hotmail.com>
2022-04-05 14:39:03 -0400

    Created the serializer model for Power

--- heroes_and_villains_project/supers/models.py
+++ heroes_and_villains_project/supers/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from super_types.models import Super_Type
 # Create your models here.
-class Super(models.Model):
-    name = models.CharField(max_length = 100)
-    alter_ego = models.CharField(max_length = 100)
-    primary_ability = models.CharField(max_length = 100)
-    secondary_ability = models.CharField(max_length = 100)
-    catchphrase = models.CharField(max_length = 100)
-    super_type = models.ForeignKey(Super_Type, on_delete = models.CASCADE)
 
 class Power(models.Model):
     name = models.CharField(max_length = 100)
+
+class Super(models.Model):
+    name = models.CharField(max_length = 100)
+    alter_ego = models.CharField(max_length = 100)
+    abilities = models.ManyToManyField(Power, many = True)
+    catchphrase = models.CharField(max_length = 100)
+    super_type = models.ForeignKey(Super_Type, on_delete = models.CASCADE)
+
 
