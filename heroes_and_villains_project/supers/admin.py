b5687300d164afdf51eb5f2be10dc16141776996
Reny Reyes <renyreyes8@hotmail.com>
2022-04-05 14:20:24 -0400

    Created a Power model + Registered model in the admin site

--- heroes_and_villains_project/supers/admin.py
+++ heroes_and_villains_project/supers/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Super
+from .models import Super,Power
 # Register your models here.
 admin.site.register(Super)
+admin.site.register(Power)
--- heroes_and_villains_project/supers/models.py
+++ heroes_and_villains_project/supers/models.py
@@ -8,3 +8,7 @@ class Super(models.Model):
     secondary_ability = models.CharField(max_length = 100)
     catchphrase = models.CharField(max_length = 100)
     super_type = models.ForeignKey(Super_Type, on_delete = models.CASCADE)
+
+class Power(models.Model):
+    name = models.CharField(max_length = 100)
+
